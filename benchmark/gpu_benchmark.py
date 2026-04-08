888c2f31237a1993c984ad4135c24b3bebf727f2
Gilad <giladsnd@gmail.com>
2026-04-08 21:53:18 +0200

    update hyper params

--- benchmark/gpu_benchmark.py
+++ benchmark/gpu_benchmark.py
@@ -558,7 +558,7 @@ def run_cifar_benchmark(device, args):
         elif cfg['randomised']:
             from optimizer.olsvered_kfac import OlsveredKFAC
             evd_freq = 20 if torch.cuda.is_available() else 50
-            opt = OlsveredKFAC(model, lr=cfg['lr'], damping=5e-3,
+            opt = OlsveredKFAC(model, lr=cfg['lr'], damping=1e-3,
                                factor_update_freq=1, inv_update_freq=evd_freq,
                                adaptive=True, adaptive_min_n=256,
                                adaptive_rank_budget=256, momentum=0.0,
