bf0d4e0a7fda53183926e05a670cf4b58419e4a4
Gilad <giladsnd@gmail.com>
2026-04-08 22:12:56 +0200

    update hyper params

--- benchmark/gpu_benchmark.py
+++ benchmark/gpu_benchmark.py
@@ -558,11 +558,11 @@ def run_cifar_benchmark(device, args):
         elif cfg['randomised']:
             from optimizer.olsvered_kfac import OlsveredKFAC
             evd_freq = 20 if torch.cuda.is_available() else 50
-            opt = OlsveredKFAC(model, lr=cfg['lr'], damping=1e-3,
+            opt = OlsveredKFAC(model, lr=cfg['lr'], damping=1e-2,
                                factor_update_freq=1, inv_update_freq=evd_freq,
                                adaptive=True, adaptive_min_n=256,
                                adaptive_rank_budget=256, momentum=0.0,
-                               grad_clip=10.0, gamma=.97)
+                               grad_clip=10.0, gamma=.999)
         else:
             from optimizer.classic_kfac import ClassicKFAC
             # ClassicKFAC needs higher damping on CIFAR-10: direct inversion is
