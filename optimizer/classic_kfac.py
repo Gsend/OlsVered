5979302047d8cb32a8a08ef849f60c07d8bfea55
Gilad <giladsnd@gmail.com>
2026-04-08 20:53:17 +0200

    update hyper params

--- optimizer/classic_kfac.py
+++ optimizer/classic_kfac.py
@@ -59,6 +59,8 @@ class ClassicKFAC(torch.optim.Optimizer):
         grad_clip: Optional[float] = None,
         gamma: float = 0.0,
     ):
+        print( 'factor_update_freq:', factor_update_freq,
+        'inv_update_freq:' ,inv_update_freq)
         defaults = dict(lr=lr, damping=damping, weight_decay=weight_decay,
                         momentum=momentum)
         params = []
