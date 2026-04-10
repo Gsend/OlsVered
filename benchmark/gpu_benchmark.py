330469ab65cf07036fe7a6a4f207de2fd2bf4474
Gilad <giladsnd@gmail.com>
2026-04-10 20:23:11 +0200

    sds

--- benchmark/gpu_benchmark.py
+++ benchmark/gpu_benchmark.py
@@ -389,14 +389,14 @@ def run_bert_benchmark(device, args):
             from optimizer.olsvered_kfac import OlsveredKFAC
             evd_freq = 50 if torch.cuda.is_available() else 100
             opt = OlsveredKFAC(model, lr=cfg['lr'], damping=5e-4,
-                               factor_update_freq=50, inv_update_freq=evd_freq,
+                               factor_update_freq=20, inv_update_freq=evd_freq,
                                adaptive=True, adaptive_min_n=128,
                                adaptive_rank_budget=128, momentum=0.0,
                                grad_clip=5.0, gamma=0.99)
         else:
             from optimizer.classic_kfac import ClassicKFAC
             opt = ClassicKFAC(model, lr=cfg['lr'], damping=5e-4,
-                              factor_update_freq=20, inv_update_freq=50,
+                              factor_update_freq=100, inv_update_freq=50,
                               momentum=0.0, grad_clip=5.0, gamma=0.9)
 
         if cfg['kfac']:
